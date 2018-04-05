8b4187bcbe593c5b2bdffd055bf69f028a556fd5
Stefano Monteiro <ste_bm@hotmail.com>
2018-04-05 12:17:17 -0600

    sticky navbar

---
+++ main.js
@@ -0,0 +1,42 @@
+const navbar = document.querySelector(".navbar");
+const navbar_height = navbar.offsetHeight;
+const navbarLogo = document.querySelector(".navbar__logo");
+const navbarItem = document.querySelectorAll(".navbar__item");
+const navbarSocialIcons = document.querySelectorAll(".navbar__social-icons");
+
+document.addEventListener("scroll", function(e) {
+  let scrollpos = window.scrollY;
+
+  if (scrollpos > navbar_height) {
+    navbar.classList.add("navbar--sticky");
+    navbarLogo.classList.add("navbar__logo--sticky");
+    navbarItem.forEach(element => {
+      element.classList.add("navbar__item--sticky");
+    });
+    navbarSocialIcons.forEach(element => {
+      element.classList.add("navbar__social-icons--sticky");
+    });
+  }
+  if (scrollpos < 10) {
+    navbar.classList.remove("navbar--sticky");
+    navbarLogo.classList.remove("navbar__logo--sticky");
+    navbarItem.forEach(element => {
+      element.classList.remove("navbar__item--sticky");
+    });
+    navbarSocialIcons.forEach(element => {
+      element.classList.remove("navbar__social-icons--sticky");
+    });
+  }
+  if (scrollpos > oldScrollpos) {
+    navbar.classList.remove("navbar--sticky");
+    navbarLogo.classList.remove("navbar__logo--sticky");
+    navbarItem.forEach(element => {
+      element.classList.remove("navbar__item--sticky");
+    });
+    navbarSocialIcons.forEach(element => {
+      element.classList.remove("navbar__social-icons--sticky");
+    });
+  }
+
+  //   console.log("Old: " + oldScrollpos + " | scrollpos: " + scrollpos);
+});
